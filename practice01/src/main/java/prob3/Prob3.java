d1ec4ef7b2f15de2fbbeb3ab72a762f85e3dbbb8
hanriverkitty <danieltls@naver.com>
2024-04-23 17:35:46 +0900

    practice01 solved

--- practice01/src/main/java/prob3/Prob3.java
+++ practice01/src/main/java/prob3/Prob3.java
@@ -7,7 +7,13 @@ public class Prob3 {
 	public static void main(String[] args) {
 
 		Scanner scanner = new Scanner(System.in);
-
+		System.out.print("숫자를 입력하세요: ");
+		int sc = scanner.nextInt();
+		int sum=0;
+		for(int i=sc;i>0;i-=2) {
+			sum+=i;
+		}
+		System.out.println("결과 값: "+ sum);
 		/* 코드 작성 */
 		
 		scanner.close();
--- practice01/src/main/java/prob5/Prob5.java
+++ practice01/src/main/java/prob5/Prob5.java
@@ -1,7 +1,25 @@
 package prob5;
-
 public class Prob5 {
 
 	public static void main(String[] args) {
+		int i=0;
+		while(i<100) {
+			String strI = String.valueOf(i);
+			int len = strI.length();
+			int cnt=0;
+			for(int j=0;j<len;j++) {
+				if(strI.charAt(j)=='3' || strI.charAt(j)=='6' || strI.charAt(j)=='9') {
+					cnt+=1;
+				}
+			}
+			if(cnt>0) {
+			System.out.print(strI+" ");
+			for(int j=0;j<cnt;j++) {
+				System.out.print("짝");
+			}
+			System.out.println();
+			}
+			i++;
+		}
 	}
 }
