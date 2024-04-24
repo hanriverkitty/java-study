d41436ceb8c2b15a94d89b5d41984d463ea471f8
hanriverkitty <danieltls@naver.com>
2024-04-24 16:33:32 +0900

    practice02-prob01 #finish

--- practice02/src/main/java/prob01/Prob01.java
+++ practice02/src/main/java/prob01/Prob01.java
@@ -9,7 +9,14 @@ public class Prob01 {
 		final int[] MONEYS = { 50000, 10000, 5000, 1000, 500, 100, 50, 10, 5, 1 };
 
 		/* 코드 작성 */
-		
+		int money = scanner.nextInt();
+		System.out.println("금액: "+money);
+		int idx=0;
+		while(idx<MONEYS.length) {
+			System.out.println(MONEYS[idx]+"원 : "+money/MONEYS[idx]+"개");
+			money=money%MONEYS[idx];
+			idx++;
+		}
 		scanner.close();
  	}
 }
