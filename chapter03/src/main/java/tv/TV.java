55d6dbf0d9997fb6e57729c75534696764170f6e
hanriverkitty <danieltls@naver.com>
2024-04-25 11:12:42 +0900

    과제 준비

---
+++ chapter03/src/main/java/tv/TV.java
@@ -0,0 +1,11 @@
+package tv;
+
+public class TV {
+	private int volume;		// 0 ~ 10
+	private int channel;	// 1 ~ 255
+	private boolean power;
+	
+	public void status() {
+		System.out.println("TV[power=]");
+	}
+}
