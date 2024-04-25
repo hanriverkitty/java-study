0b10bffb4f47523dbb15ecc12d536e30d8565f31
hanriverkitty <danieltls@naver.com>
2024-04-25 16:49:35 +0900

    practice03 prob04 #finish

--- practice03/src/main/java/prob04/StringUtil.java
+++ practice03/src/main/java/prob04/StringUtil.java
@@ -1,4 +1,12 @@
 package prob04;
 
 public class StringUtil {
+	public static String concatenate(String[] str) {
+        String concatenatedString = "";
+        for(int i=0;i<str.length;i++) {
+        	concatenatedString+=str[i];
+        }
+        return concatenatedString;
+    }
+
 }
