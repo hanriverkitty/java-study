69beb86dbb898dc5b3aab68ae248d67ec07c21e7
hanriverkitty <danieltls@naver.com>
2024-04-26 18:12:13 +0900

    practice05 prob05 #finish

--- practice05/src/main/java/prob5/MyStack.java
+++ practice05/src/main/java/prob5/MyStack.java
@@ -1,11 +1,48 @@
 package prob5;
 
-public class MyStack throws MyStackException{
+import java.util.Arrays;
+
+public class MyStack{
 	private String[] buffer;
+	private int size;
+	private int idx=-1;
 	
-	
-	public void resize() {
-		// 배열의 크기를 늘린다
-		new throw MyStackException();
+	public MyStack(int size) {
+		this.size=size;
+		buffer = new String[size];
 	}
+	public void push(String string) {
+		if (idx+1>=size) {
+			String temp[] = new String[size*2];
+			for(int i=0;i<size;i++) {
+				temp[i]=buffer[i];
+			}
+			buffer=temp;
+			size=size*2;
+		}
+		idx++;
+		buffer[idx]=string;
+		
+		
+		
+	}
+	public String pop() throws MyStackException{
+		if(idx==-1) {
+			throw new MyStackException();
+		}
+		String popString = buffer[idx];
+		idx--;
+		if(idx<0) idx=-1;
+		return popString;
+		
+	}
+	
+	public boolean isEmpty() {
+		if(idx==-1) {
+			return true;
+		}else {
+			return false;
+		}
+	}
+	
 }
