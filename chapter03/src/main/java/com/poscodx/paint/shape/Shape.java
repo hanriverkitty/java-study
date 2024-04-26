3fbea5244228117a7e2572cb8d32f56ed3726476
hanriverkitty <danieltls@naver.com>
2024-04-26 09:36:54 +0900

    package 변환 #finish

---
+++ chapter03/src/main/java/com/poscodx/paint/shape/Shape.java
@@ -0,0 +1,10 @@
+package com.poscodx.paint.shape;
+
+import com.poscodx.paint.i.Drawable;
+import com.poscodx.paint.point.Point;
+
+public abstract class Shape implements Drawable{
+	Point[] points;
+	String fillColor;
+	String lineColor;
+}
--- chapter03/src/main/java/paint/Shape.java
+++
@@ -1,7 +0,0 @@
-package paint;
-
-public abstract class Shape implements Drawable{
-	Point[] points;
-	String fillColor;
-	String lineColor;
-}
