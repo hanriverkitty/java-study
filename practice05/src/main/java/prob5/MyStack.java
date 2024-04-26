006091f17d8a5d52ef30960b37aecb5daa83fd99
hanriverkitty <danieltls@naver.com>
2024-04-26 16:08:51 +0900

    probs

--- practice05/src/main/java/prob5/MyStack.java
+++ practice05/src/main/java/prob5/MyStack.java
@@ -1,4 +1,11 @@
 package prob5;
 
-public class MyStack {
+public class MyStack throws MyStackException{
+	private String[] buffer;
+	
+	
+	public void resize() {
+		// 배열의 크기를 늘린다
+		new throw MyStackException();
+	}
 }
