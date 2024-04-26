0a945b2acd56c2d54e790a6ea4dc87332cb0bed0
hanriverkitty <danieltls@naver.com>
2024-04-26 16:43:43 +0900

    practice04 prob03 #finish

---
+++ practice04/src/main/java/prob03/Unit.java
@@ -0,0 +1,17 @@
+package prob03;
+
+public class Unit {
+	private int x;
+	private int y;
+	public Unit(int x,int y) {
+		this.x=x;
+		this.y=y;
+	}
+	
+	public void move(int x, int y) {
+		
+	}
+	public void stop() {
+		
+	}
+}
