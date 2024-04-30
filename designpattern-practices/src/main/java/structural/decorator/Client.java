8fadab3aa3937ccd4f5ae63aa0e8b963f92ae631
hanriverkitty <danieltls@naver.com>
2024-04-30 11:27:38 +0900

    Decorator #finish

---
+++ designpattern-practices/src/main/java/structural/decorator/Client.java
@@ -0,0 +1,11 @@
+package structural.decorator;
+
+public class Client {
+
+	public static void main(String[] args) {
+		System.out.println(new ConcreteComponent("Hello World").operation());
+		System.out.println(new BracesDecorator(new ConcreteComponent("Hello World")).operation());
+		System.out.println(new ParentheseDecorator(new BracesDecorator(new ConcreteComponent("Hello World"))).operation());
+	}
+
+}
