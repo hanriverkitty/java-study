2ff17f3732813076148a9bb0606d361639bd076a
hanriverkitty <danieltls@naver.com>
2024-05-01 16:46:51 +0900

    echo #finish

---
+++ network/src/main/java/echo/EchoClient.java
@@ -0,0 +1,62 @@
+package echo;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class EchoClient {
+	private static final String SERVER_IP = "127.0.0.1";
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = null;
+		Socket socket = null;
+		try {
+			sc = new Scanner(System.in);
+			socket = new Socket();
+
+			socket.connect(new InetSocketAddress(SERVER_IP, EchoServer.PORT));
+
+			PrintWriter pw = new PrintWriter(new OutputStreamWriter(socket.getOutputStream(), "utf-8"), true);
+			BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream(), "utf-8"));
+
+			while (true) {
+				System.out.print(">> ");
+				String line = sc.nextLine();
+				if ("exit".equals(line)) {
+					break;
+				}
+
+				pw.println(line);
+				String data = br.readLine(); // blocking
+				if (data == null) {
+					log("suddenly closed by server");
+					break;
+				}
+				System.out.println("<< "+data);
+			}
+		} catch (IOException e) {
+			log("error: " + e);
+		} finally {
+			try {
+				sc.close();
+				if (socket != null && !socket.isClosed()) {
+					socket.close();
+				}
+
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	private static void log(String message) {
+		System.out.println("[EchoClient]: " + message);
+	}
+
+}
