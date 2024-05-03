bbd154e826ea8c2b3ef755ac7425183c2c78c347
hanriverkitty <danieltls@naver.com>
2024-05-03 13:12:46 +0900

    socket gui #finish

---
+++ network/src/main/java/chat/gui/ChatWindow.java
@@ -0,0 +1,132 @@
+package chat.gui;
+
+import java.awt.BorderLayout;
+import java.awt.Button;
+import java.awt.Color;
+import java.awt.Frame;
+import java.awt.Panel;
+import java.awt.TextArea;
+import java.awt.TextField;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.net.SocketException;
+
+public class ChatWindow {
+
+	private Frame frame;
+	private Panel pannel;
+	private Button buttonSend;
+	private TextField textField;
+	private TextArea textArea;
+	private PrintWriter pw;
+	private BufferedReader br;
+	private Socket socket;
+
+	public ChatWindow(String name, Socket socket) {
+		frame = new Frame(name);
+		pannel = new Panel();
+		buttonSend = new Button("Send");
+		textField = new TextField();
+		textArea = new TextArea(30, 80);
+		this.socket = socket;
+
+	}
+
+	public void show() {
+
+		// Button
+		buttonSend.setBackground(Color.GRAY);
+		buttonSend.setForeground(Color.WHITE);
+		buttonSend.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent actionEvent) {
+				sendMessage();
+			}
+		});
+
+		// Textfield
+		textField.setColumns(80);
+		textField.addKeyListener(new KeyAdapter() {
+			@Override
+			public void keyPressed(KeyEvent e) {
+				char keyCode = e.getKeyChar();
+				if (keyCode == KeyEvent.VK_ENTER) {
+					sendMessage();
+				}
+
+			}
+
+		});
+
+		// Pannel
+		pannel.setBackground(Color.LIGHT_GRAY);
+		pannel.add(textField);
+		pannel.add(buttonSend);
+		frame.add(BorderLayout.SOUTH, pannel);
+
+		// TextArea
+		textArea.setEditable(false);
+		frame.add(BorderLayout.CENTER, textArea);
+
+		// Frame
+		frame.addWindowListener(new WindowAdapter() {
+			public void windowClosing(WindowEvent e) {
+				finish();
+			}
+		});
+		frame.setVisible(true);
+		frame.pack();
+
+		// IO Stream 받아오기
+		try {
+			pw = new PrintWriter(new OutputStreamWriter(socket.getOutputStream(), "utf-8"), true);
+			br = new BufferedReader(new InputStreamReader(socket.getInputStream(), "utf-8"));
+		} catch (SocketException e1) {
+			e1.printStackTrace();
+		} catch (IOException e1) {
+			e1.printStackTrace();
+		}
+		
+		// ChatClientThread 생성
+	}
+
+	private void finish() {
+		// quit 프로토콜 구현
+		// exit java application
+		// System.exit(0)
+	}
+
+	private void sendMessage() {
+		String message = textField.getText();
+		System.out.println("메세지 보내는 프로토콜을 구현!:" + message);
+		textField.setText("");
+		textField.requestFocus();
+
+		// ChatClientThread 에서 서버로 부터 받은 메시지가 있다고 치고
+		updateTextArea("마이콜:" + message);
+	}
+
+	private void updateTextArea(String message) {
+		textArea.append(message);
+		textArea.append("\n");
+	}
+
+	private class ChatClientThread extends Thread {
+
+		@Override
+		public void run() {
+			// String message = br.readline();
+			updateTextArea("마이콜:");
+		}
+	}
+}
